7c9417a6943b48e5bf533022c8a924efd222cfea
Adwaith Nair <adwaith1310@gmail.com>
2024-02-07 19:35:27 +0530

    routing update

--- client/src/Components/AddBtn.jsx
+++ client/src/Components/AddBtn.jsx
@@ -1,11 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import addTaskStyles from "../styles/addTaskBtn.module.css";
 
 //component to navigate to add task
 export default function AddBtn() {
+  //using token stored in localstorage
+  const loginToken = localStorage.getItem("loginToken");
+  const registerToken = localStorage.getItem("registerToken");
+  //assigning token
+  const token = loginToken || registerToken;
+
+  //navigating to login if token not present
+  const navigate = useNavigate();
+  const navigateToLogin = (e) => {
+    if (!token) {
+      e.preventDefault();
+      navigate("/login");
+    }
+  };
+
   return (
     <>
-      <Link to="/add" className={addTaskStyles.button}>
+      <Link
+        to="/add"
+        className={addTaskStyles.button}
+        onClick={navigateToLogin}
+      >
         Add New Task
       </Link>
     </>
